8f96632d08947d29e92e94e00374f6600e59772e
Krzysztof Płaczek <kplaczek@wi.ps.pl>
2015-12-16 13:37:20 +0100

    fixed success string, added email notification to email used as login

--- cron.php
+++ cron.php
@@ -18,36 +18,38 @@ function c($url, $postArray = null) {
     $ch = curl_init();
     $useragent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6";
 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // Prevent cURL from verifying SSL certificate
-    curl_setopt($ch, CURLOPT_FAILONERROR, TRUE); // Script should fail silently on error
-    curl_setopt($ch, CURLOPT_COOKIESESSION, TRUE); // Use cookies
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE); // Follow Location: headers
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE); // Reutrning transfer as a string
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie); // Setting cookiefile
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie); // Setting cookiejar
-    curl_setopt($ch, CURLOPT_USERAGENT, $useragent); // Setting useragent
-    curl_setopt($ch, CURLOPT_URL, $url); // Setting URL to POST
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_FAILONERROR, TRUE);
+    curl_setopt($ch, CURLOPT_COOKIESESSION, TRUE);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+    curl_setopt($ch, CURLOPT_URL, $url);
 
-    if (!is_null($useragent)) {
-        curl_setopt($ch, CURLOPT_POST, TRUE); // Setting method as POST
+    if (!is_null($postArray)) {
+        curl_setopt($ch, CURLOPT_POST, TRUE);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postArray));
     }
     $result = curl_exec($ch);
     curl_close($ch);
-
     return $result;
 }
 
 if ($freeLearningPage = c(PACKT_URL_FREE_LEARNING, $loginData)) {
-    $successString = 'Packt.user = {"uid":';
+    $successString = '"mail":"' . LOGIN . '"';
     if (strpos($freeLearningPage, $successString)) {
         preg_match('/\/freelearning-claim([^"]*)/ims', $freeLearningPage, $matches);
         preg_match('/<h2>(.*?)<\/h2>/ims', $freeLearningPage, $title);
+        preg_match('/<div class="dotd-title">.*?<\/div>.*?<div>(.*?)<\/div>/ims', $freeLearningPage, $description);
 
+        c(PACKT_URL . trim($matches[0], '/'));
 
-        echo '<pre>';
-        print_r(c(PACKT_URL . $matches[0]));
-        echo '</pre>';
+        $emailTitle = trim($title[1]);
+        $emailBody = trim($title[1]) . "\n\n" . trim($description[1]);
+
+        $x = mail(LOGIN, $emailTitle, $emailBody);
     } else {
         return FALSE;
     }
